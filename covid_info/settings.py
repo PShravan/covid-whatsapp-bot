231e6ff86300d1886c052e659caa98dfd3b7614e
PShravan <shravan.nani18@gmail.com>
2020-12-07 06:53:57 +0530

    changed settings file

--- covid_info/settings.py
+++ covid_info/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-+%03sp2+%$h$n2m-%9^$8n-f7y^((phjo=zmem_=+mle%jr$&'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['covidbot-durian.herokuapp.com']
 
 
 # Application definition
